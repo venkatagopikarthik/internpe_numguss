a53e7bb17d1651c4b12264ec76a3073f2f99de44
venkatagopikarthik <115243849+venkatagopikarthik@users.noreply.github.com>
2023-11-11 21:25:53 +0530

    GussNumberGame.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+  
+  srand((unsigned int) time(NULL));
+  
+  
+  int number = (rand() % 100) + 1; 
+ 
+  int guess = 0;
+  
+  do
+  {
+    
+    cout << "Enter Guess (1-100): ";
+    cin >> guess;
+    
+    
+    if (guess > number)
+      cout << "Guess lower!" << endl;
+    else if (guess < number)
+      cout << "Guess higher!" << endl;
+    else
+      cout << "You won!" << endl;
+      
+    
+  } while (guess != number);
+   
+  return 0;
+}
